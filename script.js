dea02e563c0c477a356dbd43e496fd86c11ff9d1
Yashasv Joshi <115701902+yashasvjoshi@users.noreply.github.com>
2023-09-06 15:47:04 +0530

    Added Milli second

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let seconds = 0;
 let winter;
 let min = 0;
 
-time.innerHTML = `00:0${min}:0${seconds}`;
+time.innerHTML = `00:0${min}:0${seconds}:${msec}`;
 
-if (seconds > 5) {
+if (seconds > 59) {
     min = min + 1
     seconds = 0
 }
@@ -19,12 +19,17 @@ start.addEventListener('click', () => {
     console.log("StopWatch Started!")
 
     winter = setInterval(() => {
-
+        msec+=1
+        if (msec > 98) {
+            seconds += 1
+            msec= 0
+        }
+        
         if (seconds > 58) {
             min = min + 1
             seconds = 0
         }
-        seconds = seconds + 1
+        // seconds = seconds + 1
         console.log(seconds)
         if (seconds < 10) {
             time.innerHTML = `00:0${min}:0${seconds}`;
@@ -33,7 +38,7 @@ start.addEventListener('click', () => {
 
             time.innerHTML = `00:0${min}:${seconds}`;
         }
-    }, 1000)
+    }, 1)
 })
 
 stop.addEventListener('click', () => {
